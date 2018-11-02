a4021d4eada723094161ebe8a4395433882a6683
Alex <a.m.koshmar@gmail.com>
2018-11-02 14:42:25 +0200

    change

--- src/main/java/com/automation/qa/pages/AdminStatsPage.java
+++ src/main/java/com/automation/qa/pages/AdminStatsPage.java
@@ -4,6 +4,6 @@ import com.automation.qa.base.TestBase;
 
 public class AdminStatsPage extends TestBase {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!!!!!");
     }
 }
